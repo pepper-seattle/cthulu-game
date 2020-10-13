d5db7abc0350662dc43d243d0bf40d60625b9fc5
glauroqj <glauro.juliani@hotmail.com>
2020-10-13 19:59:29 -0300

    fix: props to letterBox component

--- src/components/LetterBox/LetterBox.js
+++ src/components/LetterBox/LetterBox.js
@@ -4,9 +4,9 @@ import * as El from './LetterBox.style'
 // To ensure box and line are the same width
 // TODO: Make this contigent on how many letters are in the word to guess
 
-const LetterBox = ({char, key}) => {
+const LetterBox = ({char}) => {
   return (
-    <El.LetterBoxContainer key={key}>
+    <El.LetterBoxContainer>
       <p>{char}</p>
       <hr />
     </El.LetterBoxContainer>
